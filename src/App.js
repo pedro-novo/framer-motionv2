4efe26a4363f2408023bbec72b6b30fd20131437
Pedro Teixeira <pedro.novo.93@gmail.com>
2022-02-08 17:29:53 +0000

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, List, ListItem } from "@mui/material";
+import { motion } from "framer-motion";
+
+const list = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      when: "afterChildren",
+    },
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, x: -100 },
+  visible: { opacity: 1, x: 0 },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      height='100vh'
+      display='flex'
+      alignItems='center'
+      alignContent='center'
+      justifyContent='center'
+      component={motion.div}
+      variants={list}
+      initial='hidden'
+      animate='visible'
+    >
+      Framer Motion V2
+      <List>
+        <ListItem component={motion.li} variants={item}>
+          Ola
+        </ListItem>
+        <ListItem component={motion.li} variants={item}>
+          Hello
+        </ListItem>
+        <ListItem component={motion.li} variants={item}>
+          Sex
+        </ListItem>
+      </List>
+    </Box>
   );
 }
 
